b5d32fc9dbce244659b74057958c0b8c7161b329
Binaya Shrestha <BinayaShree@gmail.com>
2019-10-24 19:09:52 -0500

    setup files

--- main.c
+++ main.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 
-
-
+struct node {
+  char key[100];
+  int value;
+};
 
 int main(int argc, char const *argv[])
 {
-    
+
+  char a[100] = "Hi from teletype";
+
+  printf("%s", a);
+
     return 0;
 }
