24cb830e325532012e4f850ec75a7fc53519443c
Binaya Shrestha <BinayaShree@gmail.com>
2019-10-24 19:17:20 -0500

    adding in binaryFile

--- main.c
+++ main.c
@@ -8,9 +8,13 @@ struct node {
 int main(int argc, char const *argv[])
 {
 
+  struct node n[60];
+
+  n.key = "";
+
   char a[100] = "Hi from teletype";
 
   printf("%s", a);
 
-    return 0;
+  return 0;
 }
