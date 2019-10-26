35b445e8854f2eded0b855ee79e15b37468b9ae4
Binaya Shrestha <BinayaShree@gmail.com>
2019-10-26 16:57:06 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+
 struct node {
   char key[100];
   int value;
@@ -7,10 +8,6 @@ struct node {
 
 int main(int argc, char const *argv[])
 {
-
-  struct node n[60];
-
-  n.key = "";
 
   char a[100] = "Hi from teletype";
 
