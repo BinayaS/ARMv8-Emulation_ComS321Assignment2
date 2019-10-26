9d83608e572c3a61b76392f776cec8df60667dbf
Binaya Shrestha <BinayaShree@gmail.com>
2019-10-26 17:20:57 -0500

    updated main.c to read files from a command line argument
    - added in a fake binaryFile
    - main.c now outputs the entire file given from the command line

--- main.c
+++ main.c
@@ -13,5 +13,29 @@ int main(int argc, char const *argv[])
 
   printf("%s", a);
 
+
+  if(argc >= 2) {
+    printf("The arguments supplied are:\n");
+      for(int i = 1; i < argc; i++)
+      {
+        printf("%s\t \n", argv[i]);
+      }
+  }
+
+  FILE *file = fopen(argv[1], "r");
+
+  if(file == 0) {
+    printf("file could not be open");
+  } else {
+
+    int x;
+
+    while ((x = fgetc( file ) ) != EOF ) {
+      printf( "%c", x );
+    }
+  };
+
+
+  fclose(file);
   return 0;
 };
