7afcf8cc471cdc5e5b528ace84f853ceabe19c2a
Binaya Shrestha <BinayaShree@gmail.com>
2019-10-26 17:37:08 -0500

    seperated the reading file function into another file

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include "readFile.h"
 
 struct node {
   char key[100];
@@ -9,33 +9,11 @@ struct node {
 int main(int argc, char const *argv[])
 {
 
-  char a[100] = "Hi from teletype";
+  char a[100] = "Hi from teletype\n";
 
   printf("%s", a);
 
-
-  if(argc >= 2) {
-    printf("The arguments supplied are:\n");
-      for(int i = 1; i < argc; i++)
-      {
-        printf("%s\t \n", argv[i]);
-      }
-  }
-
-  FILE *file = fopen(argv[1], "r");
-
-  if(file == 0) {
-    printf("file could not be open");
-  } else {
-
-    int x;
-
-    while ((x = fgetc( file ) ) != EOF ) {
-      printf( "%c", x );
-    }
-  };
-
-
-  fclose(file);
+  readFile(argc, argv);
+  
   return 0;
 };
---
+++ readFile.h
@@ -0,0 +1,2 @@
+
+void readFile(int argc, char const *argv[]);
