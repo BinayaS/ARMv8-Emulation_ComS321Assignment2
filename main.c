cf1cd469d5f8a1dfe5a9c5ce147f36db8899b658
cmarquez2187 <cmarquez@iastate.edu>
2019-10-27 17:59:31 -0500

    Add opcodetable.h

--- main.c
+++ main.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
-<<<<<<< HEAD
 #include "hashtable.h"
-
-=======
 #include "readFile.h"
->>>>>>> 7afcf8cc471cdc5e5b528ace84f853ceabe19c2a
 
 struct node {
   char key[100];
@@ -19,6 +15,6 @@ int main(int argc, char const *argv[])
   printf("%s", a);
 
   readFile(argc, argv);
-  
+
   return 0;
 };
