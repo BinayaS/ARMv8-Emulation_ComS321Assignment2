a72a13cd90e67711863abfb7fadca304ce857c73
binayas <binayas@pyrite-n4.cs.iastate.edu>
2019-10-31 13:40:02 -0500

    update main.c to use the third and fourth parameters if they are given

--- main.c
+++ main.c
@@ -1,31 +1,61 @@
 #include <stdio.h>
+#include <stdlib.h>
 //#include "hashtable.h"
 //#include "opcodetable.h"
 #include "readFile.h"
 //#include "decoderStructure.h"
 
-#define MAX_ARRAY_SIZE 1000000
+#define MAX_INSTRUCTION_SIZE 1000000
 
 int main(int argc, char const *argv[])
 {
 
-  char a[100] = "Hi from teletype\n";
-  
-  int instructionArray[MAX_ARRAY_SIZE] = {-1};
+  //set local vars.
+  int mainMemorySize = 4096;
+  int stackSize = 512;
+  int instructionArray[MAX_INSTRUCTION_SIZE] = {-1};
   int counter = 0;
 
-  printf("%s", a);
+
+  //parse command line arguments if there are two or more arguments
+  if(argc >= 2) {
+    printf("The arguments supplied are:\n");
+
+    //print out all arguments
+    for(int i = 1; i < argc; i++) {
+        printf("%s\t ", argv[i]);
+    }
+    
+    printf("\n");
+    
+    //if atleast three arguments set mainMemorySize
+    if(argc >= 3) {
+      mainMemorySize = atoi(argv[2]);
+      printf("mainMemorySize = %d\n", mainMemorySize);
+    }
+    
+    //if atleas four arguments set stackSize
+    if(argc >= 4) {
+      stackSize = atoi(argv[3]);
+      printf("stackSize = %d\n", stackSize);
+    }
+    
+    //if there is not two or more arguments then print the input format
+  } else {
+      printf("<.machine file> [-m <main memory size>] [-s <stack size>]\n");
+      return 0;
+  }
 
   //read from file and put all instructions into instructionArray
   counter = readFile(argc, argv, instructionArray);
   
+  //for visual purposes print out the instructions in hex
   for(int i = 0; i < counter; i++) {
     printf("%x\n", instructionArray[i]);
   }
   
-  //TODO parse command line arguments
-  
   //TODO compare opcode by taking the instruction and shifting right till you have just the beggining and compare it to 
+  
 
   return 0;
 };
--- readFile.c
+++ readFile.c
@@ -2,27 +2,10 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-#define MAX_ARRAY_SIZE 1000000
-
-//Unused
-//http://www.firmcodes.com/write-c-program-convert-little-endian-big-endian-integer/
-unsigned int LitToBigEndian(unsigned int x)
-{
-	return (((x>>24) & 0x000000ff) | ((x>>8) & 0x0000ff00) | ((x<<8) & 0x00ff0000) | ((x<<24) & 0xff000000));
-}
-
 int readFile(int argc, char const *argv[], int instructionArray[]) {
     int c;
     int counter = 0;
     FILE *file = fopen(argv[1], "rb");
-    
-    if(argc >= 2) {
-
-    printf("The arguments supplied are:\n");
-
-    for(int i = 1; i < argc; i++) {
-        printf("%s\t \n", argv[i]);
-    }
     
     if(file == 0) {
         
@@ -36,19 +19,14 @@ int readFile(int argc, char const *argv[], int instructionArray[]) {
           c = be32toh(c);
           instructionArray[counter] = c;
           counter++;
-          printf("%02x\n", c);
+          //printf("%02x\n", c);
         }
         
-        printf("--\n");
+        //printf("--\n");
 
     };
 
     fclose(file);
-        
-
-    } else {
-        printf("No Arguments\n");
-    };
     
     return counter;
 
