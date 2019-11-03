a3aed2008974aa858227d6394714b9c7913091d3
cmarquez2187 <cmarquez@iastate.edu>
2019-11-03 15:39:05 -0600

    Make opname enum

--- main.c
+++ main.c
@@ -18,43 +18,43 @@
 */
 
 OpPair opcodeTable[] = {
-   {5, "B", B},
-   {37, "BL", B}, //End 6 bit opcodes
-   {84, "B.cond", CB},
-   {180, "CBZ", CB},
-   {181, "CBNZ", CB}, //End 8 bit opcodes
-   {580, "ADDI", I},
-   {584, "ANDI", I},
-   {712, "ORRI", I},
-   {836, "SUBI", I},
-   {840, "EORI", I},
-   {964, "SUBIS", I}, //End of 10 bit opcodes
-   {448, "STURB", D},
-   {450, "LDURB", D},
-   {960, "STURH", D},
-   {962, "LDURH", D},
-   {1104, "AND", R},
-   {1112, "ADD", R},
-   {1238, "SDIV", R},
-   {1238, "UDIV", R}, //For now, all division is unsigned, be careful
-   {1240, "MUL", R},
-   {1242, "SMULH", R},
-   {1246, "UMULH", R},
-   {1360, "ORR", R},
-   {1472, "STURW", D},
-   {1476, "LDURSW", D},
-   {1690, "LSR", R},
-   {1691, "LSL", R},
-   {1712, "BR", R},
-   {1616, "EOR", R},
-   {1624, "SUB", R},
-   {1880, "SUBS", R},
-   {1984, "STUR", D},
-   {1986, "LDUR", D},
-   {2044, "PRNL", JS},
-   {2045, "PRNT", JS},
-   {2046, "DUMP", JS},
-   {2047, "HALT", JS} //End 11 bit opcodes
+   {5, B, B},
+   {37, BL, B}, //End 6 bit opcodes
+   {84, BCOND, CB},
+   {180, CBZ, CB},
+   {181, CBNZ, CB}, //End 8 bit opcodes
+   {580, ADDI, I},
+   {584, ANDI, I},
+   {712, ORRI, I},
+   {836, SUBI, I},
+   {840, EORI, I},
+   {964, SUBIS, I}, //End of 10 bit opcodes
+   {448, STURB, D},
+   {450, LDURB, D},
+   {960, STURH, D},
+   {962, LDURH, D},
+   {1104, AND, R},
+   {1112, ADD, R},
+   {1238, SDIV, R},
+   {1238, UDIV, R}, //For now, all division is unsigned, be careful
+   {1240, MUL, R},
+   {1242, SMULH, R},
+   {1246, UMULH, R},
+   {1360, ORR, R},
+   {1472, STURW, D},
+   {1476, LDURSW, D},
+   {1690, LSR, R},
+   {1691, LSL, R},
+   {1712, BR, R},
+   {1616, EOR, R},
+   {1624, SUB, R},
+   {1880, SUBS, R},
+   {1984, STUR, D},
+   {1986, LDUR, D},
+   {2044, PRNL, JS},
+   {2045, PRNT, JS},
+   {2046, DUMP, JS},
+   {2047, HALT, JS} //End 11 bit opcodes
 };
 
 unsigned int rm = 0;
--- opcodetable.h
+++ opcodetable.h
@@ -12,10 +12,50 @@ enum EnumOpFormat {
   JS
 };
 
+enum EnumOpName {
+  B,
+  BL,   //End 6 bit opcodes
+  BCOND,
+  CBZ,
+  CBNZ,   //End 8 bit opcodes
+  ADDI,
+  ANDI,
+  ORRI,
+  SUBI,
+  EORI,
+  SUBIS,   //End of 10 bit opcodes
+  STURB,
+  LDURB,
+  STURH,
+  LDURH,
+  AND,
+  ADD,
+  SDIV,
+  UDIV,   //For now, all division is unsigned, be careful
+  MUL,
+  SMULH,
+  UMULH,
+  ORR,
+  STURW,
+  LDURSW,
+  LSR,
+  LSL,
+  BR,
+  EOR,
+  SUB,
+  SUBS,
+  STUR,
+  LDUR,
+  PRNL,
+  PRNT,
+  DUMP,  
+  HALT//End 11 bit opcodes
+};
+
 
 typedef struct OpPair {
   int opcode;
-  char opname[7];
+  enum EnumOpName opname;
   enum EnumOpFormat opformat;
 } OpPair;
 
