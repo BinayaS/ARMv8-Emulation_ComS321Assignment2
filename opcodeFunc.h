a5b59171469da4e305fa964753aa50318314c91d
Tracy <tracyle@iastate.edu>
2019-11-03 16:45:59 -0600

    fixed func prototype

--- opcodeFunc.h
+++ opcodeFunc.h
@@ -33,7 +33,7 @@ void lsl(int *des, int *reg1, int offset);
 void lsr(int *des, int *reg1, int offset);
 void umulh(int *des, int *reg1, int *reg2);
 void dump(FILE *f);
-void halt(FILE *f)
+void halt(FILE *f);
 void hexdump(FILE *f, int8_t *start, size_t size);
 char printable_char(u_int8_t c);
 
