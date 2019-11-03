2482c3f58bfb7f2f2e28d2a9d5dd84e738b80cc6
binayas <binayas@pyrite-n2.cs.iastate.edu>
2019-11-03 10:45:54 -0600

    Updated the hashtable to include opformat. Included an enum of opformats

--- opcodetable.h
+++ opcodetable.h
@@ -1,9 +1,22 @@
  #ifndef OPCODETABLE_H_
  #define OPCODETABLE_H_
 
+enum EnumOpFormat {
+  DEFAULT,
+  R,
+  I,
+  D,
+  B,
+  CB,
+  IW,
+  JS
+};
+
+
 typedef struct OpPair {
   int opcode;
   char opname[7];
+  enum EnumOpFormat opformat; 
 } OpPair;
 
 int searchTable(int opcode, OpPair* opcodeTable);
